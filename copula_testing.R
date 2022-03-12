98528965f8238cc1b273025fb0d4794d2f040d88
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-03-12 23:37:45 +0800

    Fix testing vals

--- copula_testing.R
+++ copula_testing.R
@@ -2,7 +2,7 @@ setwd("brms")
 devtools::load_all()
 
 bdata_bin = data.frame(n = c(2,1,4,5,1), d = c(5,5,5,5,5),
-                       y = rep(1, 5), c = rep(1,5))
+                       y = rnorm(n = 5,mean = 4,sd=10), c = c(2,1,3,2,4))
 
 mn <- bf(y ~ 1, family = gaussian())
 mp <- bf(c ~ 1, family = poisson())
